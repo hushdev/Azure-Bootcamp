cd496639ceb9c4475ef610df3b0cf99471f48f3f
hushdev <ildar.hushdev@gmail.com>
2022-09-21 10:27:17 +0200

    Task #5 completed - blob storage

---
+++ 5_blob_storage/script.js
@@ -0,0 +1,127 @@
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+// Declare UI elements
+const createContainerButton = document.getElementById("create-container-button");
+const deleteContainerButton = document.getElementById("delete-container-button");
+const selectButton = document.getElementById("select-button");
+const fileInput = document.getElementById("file-input");
+const listButton = document.getElementById("list-button");
+const deleteButton = document.getElementById("delete-button");
+const status = document.getElementById("status");
+const fileList = document.getElementById("file-list");
+
+const reportStatus = message => {
+    status.innerHTML += `${message}<br/>`;
+    status.scrollTop = status.scrollHeight;
+}
+
+
+// Add your storage account info
+const blobSasUrl = "https://test4storage123.blob.core.windows.net/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2022-12-21T17:16:30Z&st=2022-09-21T08:16:30Z&spr=https,http&sig=CLvOLmVaye8rimd%2F4ha9XXbrTFYEJGs4S70kXP3kA9w%3D";
+
+// Create a new BlobServiceClient
+const blobServiceClient = new BlobServiceClient(blobSasUrl);
+
+// Create a unique name for the container by 
+// appending the current time to the file name
+const containerName = "container" + new Date().getTime();
+
+// Get a container client from the BlobServiceClient
+const containerClient = blobServiceClient.getContainerClient(containerName);
+
+
+// Create and delete a storage container
+const createContainer = async () => {
+    try {
+        reportStatus(`Creating container "${containerName}"...`);
+        await containerClient.create();
+        reportStatus(`Done. URL:${containerClient.url}`);
+    } catch (error) {
+        reportStatus(error.message);
+    }
+};
+
+const deleteContainer = async () => {
+    try {
+        reportStatus(`Deleting container "${containerName}"...`);
+        await containerClient.delete();
+        reportStatus(`Done.`);
+    } catch (error) {
+        reportStatus(error.message);
+    }
+};
+
+createContainerButton.addEventListener("click", createContainer);
+deleteContainerButton.addEventListener("click", deleteContainer);
+
+
+// List BLOBS
+const listFiles = async () => {
+    fileList.size = 0;
+    fileList.innerHTML = "";
+    try {
+        reportStatus("Retrieving file list...");
+        let iter = containerClient.listBlobsFlat();
+        let blobItem = await iter.next();
+        while (!blobItem.done) {
+            fileList.size += 1;
+            fileList.innerHTML += `<option>${blobItem.value.name}</option>`;
+
+
+            blobItem = await iter.next();
+        }
+        if (fileList.size > 0) {
+            reportStatus("Done.");
+        } else {
+            reportStatus("The container does not contain any files.");
+        }
+    } catch (error) {
+        reportStatus(error.message);
+    }
+};
+
+listButton.addEventListener("click", listFiles);
+
+
+//Upload blobs to a container
+const uploadFiles = async () => {
+    try {
+        reportStatus("Uploading files...");
+        const promises = [];
+        for (const file of fileInput.files) {
+            const blockBlobClient = containerClient.getBlockBlobClient(file.name);
+            promises.push(blockBlobClient.uploadBrowserData(file));
+        }
+        await Promise.all(promises);
+        reportStatus("Done.");
+        listFiles();
+    }
+    catch (error) {
+        reportStatus(error.message);
+    }
+}
+
+selectButton.addEventListener("click", () => fileInput.click());
+fileInput.addEventListener("change", uploadFiles);
+
+
+//Delete blobs
+const deleteFiles = async () => {
+    try {
+        if (fileList.selectedOptions.length > 0) {
+            reportStatus("Deleting files...");
+            for (const option of fileList.selectedOptions) {
+                await containerClient.deleteBlob(option.text);
+            }
+            reportStatus("Done.");
+            listFiles();
+        } else {
+            reportStatus("No files selected.");
+        }
+    } catch (error) {
+        reportStatus(error.message);
+    }
+};
+
+deleteButton.addEventListener("click", deleteFiles);
+
